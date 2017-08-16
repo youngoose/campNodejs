8163ed73cf6a80759225b23e368dc4e887674974
Younghoon Jo <edwardjo811@gmail.com>
2017-08-16 01:18:23 +0000

    add {useMongoClient: true} to app.js / modify bootstrap version to 3.3.7

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
     
 var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v11Deployed"    
-mongoose.connect(url);    
+mongoose.connect(url, {useMongoClient: true});    
 
 mongoose.Promise = global.Promise;
 
