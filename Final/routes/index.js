3fd847f9ab642f124261ef3063fc249846b08d57
Younghoon Jo <edwardjo811@gmail.com>
2017-08-19 03:32:09 +0000

    complete user profile function

--- Final/routes/index.js
+++ Final/routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campground");
 
 //root route
 router.get("/", function(req, res){
@@ -15,18 +16,27 @@ router.get("/register", function(req, res){
 
 //handle sign up logic
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
+    var newUser = new User(
+        {
+            username: req.body.username, 
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            avatar: req.body.avatar
+        });
+        
     if(req.body.adminCode === 'secret12345'){
         newUser.isAdmin = true;
     }
+    
     User.register(newUser, req.body.password, function(err, user){
         if(err){
             console.log(err);
             return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Successfully Signed Up! Nice to meet you " + user.username);
-           res.redirect("/campgrounds"); 
+          req.flash("success", "Successfully Signed Up! Nice to meet you " + user.username);
+          res.redirect("/campgrounds"); 
         });
     });
 });
@@ -40,7 +50,9 @@ router.get("/login", function(req, res){
 router.post("/login", passport.authenticate("local", 
     {
         successRedirect: "/campgrounds",
-        failureRedirect: "/login"
+        failureRedirect: "/login",
+        failureFlash: true,
+        successFlash: "Welcome to YelpCamp!"
     }), function(req, res){
 });
 
@@ -51,6 +63,21 @@ router.get("/logout", function(req, res){
    res.redirect("/campgrounds");
 });
 
-
+// user profile
+router.get("/users/:id", function(req, res) {
+   User.findById(req.params.id, function(err, foundUser){
+       if(err){
+           req.flash("error", "Something went wrong.");
+           res.redirect("/");
+       }
+       Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds){
+          if(err){
+            req.flash("error", "Something went wrong.");
+            res.redirect("/");
+          }
+          res.render("users/show", {user: foundUser, campgrounds: campgrounds}); 
+       });
+   }); 
+});
 
 module.exports = router;
